097240acdddfdf085051299aea389927be307594
David <22782132+fossinating@users.noreply.github.com>
2025-02-22 20:04:08 -0500

    Created a basic ink particle burst effect for landing

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -4,7 +4,10 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+var was_on_floor := true
+
 @onready var ink_trail_particles: GPUParticles3D = get_node("Ink Trail Particles")
+@onready var ink_burst_particles_scene: PackedScene = preload("res://entities/effects/ink_burst_particles.tscn")
 
 
 func _physics_process(delta: float) -> void:
@@ -17,10 +20,18 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		print("jumping")
+		#add_child(ink_burst_particles_scene.instantiate())
+
+	# Handle landing
+
+	if (not was_on_floor) and is_on_floor():
+		print("landing")
+		add_child(ink_burst_particles_scene.instantiate())
+
 
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	print(input_dir)
 	var direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -28,5 +39,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	was_on_floor = is_on_floor()
 
 	move_and_slide()
