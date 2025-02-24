b783701abe60a1e8216b8f05522834a8317b5715
David <22782132+fossinating@users.noreply.github.com>
2025-02-24 02:27:53 -0500

    Create Item Delivery Points

--- entities/items/item_delivery_point.gd
+++ entities/items/item_delivery_point.gd
@@ -1,7 +1,8 @@
 extends Area3D
+class_name ItemDeliveryPoint
 
 
-var delivered_item
+var delivered_item: GrabbableItem
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func deliver(item) -> void:
@@ -10,4 +11,21 @@ func deliver(item) -> void:
 	
 func _process(delta: float) -> void:
 	if delivered_item:
-		delivered_item.position = -1 * min(2*delta, delivered_item.position.length()) * delivered_item.position.normalized()
+		if delivered_item.position.length() > 0.05:
+			delivered_item.position += -1 * min(2*delta, delivered_item.position.length()) * delivered_item.position.normalized()
+		else:
+			delivered_item.position = Vector3.ZERO
+
+
+func _on_area_entered(area: Area3D):
+	# Only do something if the delivery hasn't already been made
+	# and if the area3d is a GrabbableItem
+	if (not delivered_item) and is_instance_of(area, GrabbableItem):
+		# Also check to make sure it is the right item
+		if (area as GrabbableItem).delivery_point == self:
+			delivered_item = area
+			if (area as GrabbableItem).carrier:
+				(area as GrabbableItem).carrier.drop_item((area as GrabbableItem))
+			delivered_item.reparent(self)
+			delivered_item.top_level = false
+			delivered_item.collision_shape.set_deferred("disabled", true)
--- entities/player/components/item_grabber/item_grabber.gd
+++ entities/player/components/item_grabber/item_grabber.gd
@@ -1,19 +1,11 @@
 extends Area3D
+class_name ItemGrabber
 
 
 var current_item
 var hovered_item
 
-const MAX_HOLD_DISTANCE = 0.5
-
-
-func _physics_process(delta: float) -> void:
-	if current_item:
-		var dist = current_item.global_position.distance_to(global_position)
-		if dist > MAX_HOLD_DISTANCE:
-			current_item.global_position = lerp(current_item.global_position, 
-			global_position + (current_item.global_position - global_position) / dist * MAX_HOLD_DISTANCE, 0.9)
-			
+const MAX_HOLD_DISTANCE = 0.5		
 
 
 func get_interaction_text() -> String:
@@ -37,26 +29,27 @@ func interact() -> void:
 		pickup_item(hovered_item)
 
 func _on_area_entered(area: Area3D) -> void:
-	Signals.add_interaction.emit(self)
-	hovered_item = area
+	if area != current_item:
+		Signals.add_interaction.emit(self)
+		hovered_item = area
 
 func _on_area_exited(area: Area3D) -> void:
-	Signals.remove_interaction.emit(self)
-	if hovered_item == area:
-		hovered_item = null
+	if area != current_item:
+		Signals.remove_interaction.emit(self)
+		if hovered_item == area:
+			hovered_item = null
 
-func pickup_item(item: Area3D) -> void:
-	item.collision_layer = 0
+func pickup_item(item: GrabbableItem) -> void:
 	item.reparent(self)
+	item.on_pickup(self)
 	item.top_level = true
 	current_item = item
 	Signals.add_interaction.emit(self)
 
-func drop_item(item: Area3D) -> void:
-	item.collision_layer = 1 << 3
+func drop_item(item: GrabbableItem) -> void:
 	item.reparent(get_parent().get_parent())
+	current_item.on_drop(self)
 	item.top_level = false
-	hovered_item = item
 	current_item = null
 	Signals.remove_interaction.emit(self)
 	
