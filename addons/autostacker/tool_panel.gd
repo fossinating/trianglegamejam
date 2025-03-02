46be0fc5daaa57ff67d3c9d3a1a4bec3449aa99b
bbccCT <bbcc@unc.edu>
2025-03-02 10:12:36 -0500

    Hide non-stacker elements of plugin so autostacker can be used normally

--- addons/autostacker/tool_panel.gd
+++ addons/autostacker/tool_panel.gd
@@ -203,177 +203,174 @@ func _on_line_edit_max_z_text_changed(new_text: String) -> void:
 		box.text = str(rangeZ.y)
 	box.caret_column = new_text.length()
 
-
-var negativeCorner := Vector2(0.0, 0.0)
-var positiveCorner := Vector2(0.0, 0.0)
-var tiling_interval := Vector2(0.0, 0.0)
-
-func _on_tile_button_pressed() -> void:
-	var selection = EditorInterface.get_selection()
-	
-	undo_redo.create_action("Autostacker: Tile Nodes")
-	
-	var startingPosition := Vector3.ZERO
-	
-	var positionAverage := Vector3.ZERO
-	var selectedCount := 0
-	for node in selection.get_selected_nodes():
-		if node is Node3D:
-			positionAverage += node.position
-			selectedCount += 1
-	positionAverage /= selectedCount
-	startingPosition = positionAverage
-	
-	for i in range(negativeCorner.x, positiveCorner.x):
-		for j in range(negativeCorner.y, positiveCorner.y):
-			for node in selection.get_selected_nodes():
-				if node is Node3D:
-					var new_node := node.duplicate()
-					add_child(new_node)
-					new_node.owner = self.get_parent()
-					var pos = Vector3(new_node.position.x + i * tiling_interval.x, new_node.position.y, new_node.position.z + j * tiling_interval.y)
-					
-					undo_redo.add_do_method(node, "duplicate")
-					undo_redo.add_do_property(new_node, "position", pos)
-					undo_redo.add_undo_property(new_node, "position", new_node.position)
-					undo_redo.add_undo_method(node, "duplicate")
-			
-	undo_redo.commit_action()
-
-func _on_line_edit_size_x_text_changed(new_text: String) -> void:
-	var box := $HBoxTilingSize/LineEditSizeX
-	if new_text == "":
-		tiling_interval.x = 0.0
-	elif new_text == "-":
-		tiling_interval.x = 0.0
-	elif new_text == ".":
-		tiling_interval.x = 0.0
-		box.text = "0."
-		new_text = "0."
-	elif new_text.length() >= 2 and new_text.substr(0, new_text.length() - 1).is_valid_float() and new_text[new_text.length() - 1] == "-":
-		tiling_interval.x = float(new_text.substr(0, new_text.length() - 1)) * -1.0
-		if float(new_text.substr(0, new_text.length() - 1)) == 0:
-			tiling_interval.x = 0
-			box.text = str("-", new_text.substr(0, new_text.length() - 1))
-		else:
-			box.text = str(tiling_interval.x)
-	elif only_float(float(box.text), new_text)[1]:
-		tiling_interval.x = float(box.text)
-	else:
-		box.text = str(tiling_interval.x)
-	box.caret_column = new_text.length()
-
-func _on_line_edit_size_y_text_changed(new_text: String) -> void:
-	var box := $HBoxTilingSize/LineEditSizeY
-	if new_text == "":
-		tiling_interval.y = 0.0
-	elif new_text == "-":
-		tiling_interval.y = 0.0
-	elif new_text == ".":
-		tiling_interval.y = 0.0
-		box.text = "0."
-		new_text = "0."
-	elif new_text.length() >= 2 and new_text.substr(0, new_text.length() - 1).is_valid_float() and new_text[new_text.length() - 1] == "-":
-		tiling_interval.y = float(new_text.substr(0, new_text.length() - 1)) * -1.0
-		if float(new_text.substr(0, new_text.length() - 1)) == 0:
-			tiling_interval.y = 0
-			box.text = str("-", new_text.substr(0, new_text.length() - 1))
-		else:
-			box.text = str(tiling_interval.y)
-	elif only_float(float(box.text), new_text)[1]:
-		tiling_interval.y = float(box.text)
-	else:
-		box.text = str(tiling_interval.y)
-	box.caret_column = new_text.length()
-
-func _on_line_edit_negative_x_text_changed(new_text: String) -> void:
-	var box := $HBoxTilingNegative/LineEditNegativeX
-	if new_text == "":
-		negativeCorner.x = 0.0
-	elif new_text == "-":
-		negativeCorner.x = 0.0
-	elif new_text == ".":
-		negativeCorner.x = 0.0
-		box.text = "0."
-		new_text = "0."
-	elif new_text.length() >= 2 and new_text.substr(0, new_text.length() - 1).is_valid_float() and new_text[new_text.length() - 1] == "-":
-		negativeCorner.x = float(new_text.substr(0, new_text.length() - 1)) * -1.0
-		if float(new_text.substr(0, new_text.length() - 1)) == 0:
-			negativeCorner.x = 0
-			box.text = str("-", new_text.substr(0, new_text.length() - 1))
-		else:
-			box.text = str(negativeCorner.x)
-	elif only_float(float(box.text), new_text)[1]:
-		negativeCorner.x = float(box.text)
-	else:
-		box.text = str(negativeCorner.x)
-	box.caret_column = new_text.length()
-
-func _on_line_edit_negative_y_text_changed(new_text: String) -> void:
-	var box := $HBoxTilingNegative/LineEditNegativeY
-	if new_text == "":
-		negativeCorner.y = 0.0
-	elif new_text == "-":
-		negativeCorner.y = 0.0
-	elif new_text == ".":
-		negativeCorner.y = 0.0
-		box.text = "0."
-		new_text = "0."
-	elif new_text.length() >= 2 and new_text.substr(0, new_text.length() - 1).is_valid_float() and new_text[new_text.length() - 1] == "-":
-		negativeCorner.y = float(new_text.substr(0, new_text.length() - 1)) * -1.0
-		if float(new_text.substr(0, new_text.length() - 1)) == 0:
-			negativeCorner.y = 0
-			box.text = str("-", new_text.substr(0, new_text.length() - 1))
-		else:
-			box.text = str(negativeCorner.y)
-	elif only_float(float(box.text), new_text)[1]:
-		negativeCorner.y = float(box.text)
-	else:
-		box.text = str(negativeCorner.y)
-	box.caret_column = new_text.length()
-
-func _on_line_edit_positive_x_text_changed(new_text: String) -> void:
-	var box := $HBoxTilingPositive/LineEditPositiveX
-	if new_text == "":
-		positiveCorner.x = 0.0
-	elif new_text == "-":
-		positiveCorner.x = 0.0
-	elif new_text == ".":
-		positiveCorner.x = 0.0
-		box.text = "0."
-		new_text = "0."
-	elif new_text.length() >= 2 and new_text.substr(0, new_text.length() - 1).is_valid_float() and new_text[new_text.length() - 1] == "-":
-		positiveCorner.x = float(new_text.substr(0, new_text.length() - 1)) * -1.0
-		if float(new_text.substr(0, new_text.length() - 1)) == 0:
-			positiveCorner.x = 0
-			box.text = str("-", new_text.substr(0, new_text.length() - 1))
-		else:
-			box.text = str(positiveCorner.x)
-	elif only_float(float(box.text), new_text)[1]:
-		positiveCorner.x = float(box.text)
-	else:
-		box.text = str(positiveCorner.x)
-	box.caret_column = new_text.length()
-
-func _on_line_edit_positive_y_text_changed(new_text: String) -> void:
-	var box := $HBoxTilingPositive/LineEditPositiveY
-	if new_text == "":
-		positiveCorner.y = 0.0
-	elif new_text == "-":
-		positiveCorner.y = 0.0
-	elif new_text == ".":
-		positiveCorner.y = 0.0
-		box.text = "0."
-		new_text = "0."
-	elif new_text.length() >= 2 and new_text.substr(0, new_text.length() - 1).is_valid_float() and new_text[new_text.length() - 1] == "-":
-		positiveCorner.y = float(new_text.substr(0, new_text.length() - 1)) * -1.0
-		if float(new_text.substr(0, new_text.length() - 1)) == 0:
-			positiveCorner.y = 0
-			box.text = str("-", new_text.substr(0, new_text.length() - 1))
-		else:
-			box.text = str(positiveCorner.y)
-	elif only_float(float(box.text), new_text)[1]:
-		positiveCorner.y = float(box.text)
-	else:
-		box.text = str(positiveCorner.y)
-	box.caret_column = new_text.length()
+#
+#var negativeCorner := Vector2(0.0, 0.0)
+#var positiveCorner := Vector2(0.0, 0.0)
+#var tiling_interval := Vector2(0.0, 0.0)
+#
+#func _on_tile_button_pressed() -> void:
+	#var selection = EditorInterface.get_selection()
+	#
+	#undo_redo.create_action("Autostacker: Tile Nodes")
+	#
+	#var startingPosition := Vector3.ZERO
+	#
+	#if !load(tilePath) :
+		#push_warning("Error loading scene ", tilePath)
+	#else:
+		#var tileNode = load(tilePath)
+		#for i in range(negativeCorner.x, positiveCorner.x):
+			#for j in range(negativeCorner.y, positiveCorner.y):
+				#if tileNode is Node3D:
+					#var new_node: Node3D = tileNode.new()
+					#get_tree().edited_scene_root.add_child(new_node)
+					#var pos = Vector3(new_node.position.x + i * tiling_interval.x, new_node.position.y, new_node.position.z + j * tiling_interval.y)
+					#
+					#undo_redo.add_do_method(new_node, "new")
+					#undo_redo.add_do_property(new_node, "position", pos)
+					#undo_redo.add_undo_property(new_node, "position", new_node.position)
+					#undo_redo.add_undo_method(new_node, "new")
+			#
+	#undo_redo.commit_action()
+#
+#func _on_line_edit_size_x_text_changed(new_text: String) -> void:
+	#var box := $HBoxTilingSize/LineEditSizeX
+	#if new_text == "":
+		#tiling_interval.x = 0.0
+	#elif new_text == "-":
+		#tiling_interval.x = 0.0
+	#elif new_text == ".":
+		#tiling_interval.x = 0.0
+		#box.text = "0."
+		#new_text = "0."
+	#elif new_text.length() >= 2 and new_text.substr(0, new_text.length() - 1).is_valid_float() and new_text[new_text.length() - 1] == "-":
+		#tiling_interval.x = float(new_text.substr(0, new_text.length() - 1)) * -1.0
+		#if float(new_text.substr(0, new_text.length() - 1)) == 0:
+			#tiling_interval.x = 0
+			#box.text = str("-", new_text.substr(0, new_text.length() - 1))
+		#else:
+			#box.text = str(tiling_interval.x)
+	#elif only_float(float(box.text), new_text)[1]:
+		#tiling_interval.x = float(box.text)
+	#else:
+		#box.text = str(tiling_interval.x)
+	#box.caret_column = new_text.length()
+#
+#func _on_line_edit_size_y_text_changed(new_text: String) -> void:
+	#var box := $HBoxTilingSize/LineEditSizeY
+	#if new_text == "":
+		#tiling_interval.y = 0.0
+	#elif new_text == "-":
+		#tiling_interval.y = 0.0
+	#elif new_text == ".":
+		#tiling_interval.y = 0.0
+		#box.text = "0."
+		#new_text = "0."
+	#elif new_text.length() >= 2 and new_text.substr(0, new_text.length() - 1).is_valid_float() and new_text[new_text.length() - 1] == "-":
+		#tiling_interval.y = float(new_text.substr(0, new_text.length() - 1)) * -1.0
+		#if float(new_text.substr(0, new_text.length() - 1)) == 0:
+			#tiling_interval.y = 0
+			#box.text = str("-", new_text.substr(0, new_text.length() - 1))
+		#else:
+			#box.text = str(tiling_interval.y)
+	#elif only_float(float(box.text), new_text)[1]:
+		#tiling_interval.y = float(box.text)
+	#else:
+		#box.text = str(tiling_interval.y)
+	#box.caret_column = new_text.length()
+#
+#func _on_line_edit_negative_x_text_changed(new_text: String) -> void:
+	#var box := $HBoxTilingNegative/LineEditNegativeX
+	#if new_text == "":
+		#negativeCorner.x = 0.0
+	#elif new_text == "-":
+		#negativeCorner.x = 0.0
+	#elif new_text == ".":
+		#negativeCorner.x = 0.0
+		#box.text = "0."
+		#new_text = "0."
+	#elif new_text.length() >= 2 and new_text.substr(0, new_text.length() - 1).is_valid_float() and new_text[new_text.length() - 1] == "-":
+		#negativeCorner.x = float(new_text.substr(0, new_text.length() - 1)) * -1.0
+		#if float(new_text.substr(0, new_text.length() - 1)) == 0:
+			#negativeCorner.x = 0
+			#box.text = str("-", new_text.substr(0, new_text.length() - 1))
+		#else:
+			#box.text = str(negativeCorner.x)
+	#elif only_float(float(box.text), new_text)[1]:
+		#negativeCorner.x = float(box.text)
+	#else:
+		#box.text = str(negativeCorner.x)
+	#box.caret_column = new_text.length()
+#
+#func _on_line_edit_negative_y_text_changed(new_text: String) -> void:
+	#var box := $HBoxTilingNegative/LineEditNegativeY
+	#if new_text == "":
+		#negativeCorner.y = 0.0
+	#elif new_text == "-":
+		#negativeCorner.y = 0.0
+	#elif new_text == ".":
+		#negativeCorner.y = 0.0
+		#box.text = "0."
+		#new_text = "0."
+	#elif new_text.length() >= 2 and new_text.substr(0, new_text.length() - 1).is_valid_float() and new_text[new_text.length() - 1] == "-":
+		#negativeCorner.y = float(new_text.substr(0, new_text.length() - 1)) * -1.0
+		#if float(new_text.substr(0, new_text.length() - 1)) == 0:
+			#negativeCorner.y = 0
+			#box.text = str("-", new_text.substr(0, new_text.length() - 1))
+		#else:
+			#box.text = str(negativeCorner.y)
+	#elif only_float(float(box.text), new_text)[1]:
+		#negativeCorner.y = float(box.text)
+	#else:
+		#box.text = str(negativeCorner.y)
+	#box.caret_column = new_text.length()
+#
+#func _on_line_edit_positive_x_text_changed(new_text: String) -> void:
+	#var box := $HBoxTilingPositive/LineEditPositiveX
+	#if new_text == "":
+		#positiveCorner.x = 0.0
+	#elif new_text == "-":
+		#positiveCorner.x = 0.0
+	#elif new_text == ".":
+		#positiveCorner.x = 0.0
+		#box.text = "0."
+		#new_text = "0."
+	#elif new_text.length() >= 2 and new_text.substr(0, new_text.length() - 1).is_valid_float() and new_text[new_text.length() - 1] == "-":
+		#positiveCorner.x = float(new_text.substr(0, new_text.length() - 1)) * -1.0
+		#if float(new_text.substr(0, new_text.length() - 1)) == 0:
+			#positiveCorner.x = 0
+			#box.text = str("-", new_text.substr(0, new_text.length() - 1))
+		#else:
+			#box.text = str(positiveCorner.x)
+	#elif only_float(float(box.text), new_text)[1]:
+		#positiveCorner.x = float(box.text)
+	#else:
+		#box.text = str(positiveCorner.x)
+	#box.caret_column = new_text.length()
+#
+#func _on_line_edit_positive_y_text_changed(new_text: String) -> void:
+	#var box := $HBoxTilingPositive/LineEditPositiveY
+	#if new_text == "":
+		#positiveCorner.y = 0.0
+	#elif new_text == "-":
+		#positiveCorner.y = 0.0
+	#elif new_text == ".":
+		#positiveCorner.y = 0.0
+		#box.text = "0."
+		#new_text = "0."
+	#elif new_text.length() >= 2 and new_text.substr(0, new_text.length() - 1).is_valid_float() and new_text[new_text.length() - 1] == "-":
+		#positiveCorner.y = float(new_text.substr(0, new_text.length() - 1)) * -1.0
+		#if float(new_text.substr(0, new_text.length() - 1)) == 0:
+			#positiveCorner.y = 0
+			#box.text = str("-", new_text.substr(0, new_text.length() - 1))
+		#else:
+			#box.text = str(positiveCorner.y)
+	#elif only_float(float(box.text), new_text)[1]:
+		#positiveCorner.y = float(box.text)
+	#else:
+		#box.text = str(positiveCorner.y)
+	#box.caret_column = new_text.length()
+#
+#var tilePath := ""
+#func _on_line_edit_tile_path_text_changed(new_text: String) -> void:
+	#tilePath = new_text
